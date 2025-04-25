afdb4d27b7d07acee06ee62d0e8f2d602e0ffea6
Hadi <969hadiahmed@gmail.com>
2025-04-25 17:30:36 +0530

    feat: apollo server setup

--- job-board/server/server.js
+++ job-board/server/server.js
@@ -1,14 +1,21 @@
 import cors from 'cors';
 import express from 'express';
 import { authMiddleware, handleLogin } from './auth.js';
+import { ApolloServer } from '@apollo/server';
+import { expressMiddleware as apolloMiddleware } from '@apollo/server/express4';
 
 const PORT = 9000;
 
 const app = express();
+const apolloServe = new ApolloServer({/*typeDefs, resolvers*/ });
+
 app.use(cors(), express.json(), authMiddleware);
+app.use('/graphql', apolloMiddleware(apolloServe));
 
 app.post('/login', handleLogin);
 
+
+await apolloServe.start();
 app.listen({ port: PORT }, () => {
   console.log(`Server running on port ${PORT}`);
 });
