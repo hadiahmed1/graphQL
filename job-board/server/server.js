4845f8f6b45718aa31c21a177bd18e5a53a7477d
Hadi <969hadiahmed@gmail.com>
2025-04-25 17:56:01 +0530

    feat: graphQL setup

--- job-board/server/server.js
+++ job-board/server/server.js
@@ -3,19 +3,23 @@ import express from 'express';
 import { authMiddleware, handleLogin } from './auth.js';
 import { ApolloServer } from '@apollo/server';
 import { expressMiddleware as apolloMiddleware } from '@apollo/server/express4';
+import { readFile } from "node:fs/promises"
+import { resolvers } from './resolver.js';
+import { isUtf8 } from 'node:buffer';
 
 const PORT = 9000;
 
 const app = express();
-const apolloServe = new ApolloServer({/*typeDefs, resolvers*/ });
 
 app.use(cors(), express.json(), authMiddleware);
-app.use('/graphql', apolloMiddleware(apolloServe));
-
 app.post('/login', handleLogin);
 
-
+//apolo server setup
+const typeDefs = await readFile('./schema.graphql', 'utf8');
+const apolloServe = new ApolloServer({ typeDefs, resolvers });
 await apolloServe.start();
+app.use('/graphql', apolloMiddleware(apolloServe));
+
 app.listen({ port: PORT }, () => {
   console.log(`Server running on port ${PORT}`);
 });
