19a0e9451a0c92a6e33b08f9b3bb5a3f92d9343b
Hadi <969hadiahmed@gmail.com>
2025-04-26 12:25:16 +0530

    feat: jobs resolver: fetching data from DB

--- job-board/server/resolver.js
+++ job-board/server/resolver.js
@@ -1,12 +1,19 @@
-
+import { getJobs } from "./db/jobs.js"
 export const resolvers = {
-    Query : {
-        job:()=>{
+    Query: {
+        jobs: async () => {
+            const jobs = await getJobs();
+            return jobs;
+        },
+        job: () => {
             return {
                 id: "test-ID",
                 title: "Job Title",
                 description: "Job DESC"
             }
         }
+    },
+    Job: {
+        date: (job) => job.createdAt.slice(0,10),
     }
 }
