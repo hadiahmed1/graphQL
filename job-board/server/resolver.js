9f7b43b8f58a07ebf5a6ca714d43ba7033713ca7
Hadi <969hadiahmed@gmail.com>
2025-04-28 17:57:12 +0530

    error: handelling

--- job-board/server/resolver.js
+++ job-board/server/resolver.js
@@ -1,10 +1,20 @@
 import { getJobs, getJob, getJobsByCompany } from "./db/jobs.js"
 import { getCompany } from "./db/companies.js";
+import { GraphQLError } from "graphql";
+
 export const resolvers = {
     Query: {
         jobs: () => getJobs(),
         job: (_root, args) => getJob(args.id),
-        company: (_root, args) => getCompany(args.id)
+        company: async (_root, args) =>{
+            const company = await getCompany(args.id);
+            if(!company){ 
+                throw new GraphQLError("Company not found with id:"+args.id, {
+                    extensions: {code: "NOT_FOUND"}
+                })
+            }
+            return company;
+        }    
     },
     Job: {
         date: (job) => job.createdAt.slice(0, 10),
