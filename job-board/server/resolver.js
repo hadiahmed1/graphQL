86570778854491d3c9f282c791714c3eaaab8343
Hadi <969hadiahmed@gmail.com>
2025-04-29 13:35:14 +0530

    feat: Authentication

--- job-board/server/resolver.js
+++ job-board/server/resolver.js
@@ -1,35 +1,66 @@
-import { getJobs, getJob, getJobsByCompany } from "./db/jobs.js"
+import { getJobs, getJob, getJobsByCompany, createJob, deleteJob, updateJob } from "./db/jobs.js"
 import { getCompany } from "./db/companies.js";
-import { GraphQLError } from "graphql";
+import { assertInputObjectType, GraphQLError } from "graphql";
+import { UnauthorizedError } from "express-jwt";
+import { getUser } from "./db/users.js";
 
 export const resolvers = {
     Query: {
         jobs: () => getJobs(),
         job: (_root, args) => getJob(args.id),
-        company: async (_root, args) =>{
+        company: async (_root, args) => {
             const company = await getCompany(args.id);
-            if(!company)
-                throw notFoundError("No company with ID:"+args.id)
-            
+            if (!company)
+                throw notFoundError("No company with ID:" + args.id)
+
             return company;
-        }    
+        }
     },
     Job: {
         date: (job) => job.createdAt.slice(0, 10),
         company: async (job) => {
-            const job = await getCompany(job.companyId);
-            if(!job) 
-                throw notFoundError("No Job with ID:"+args.id)
-            return job;
+            const company = await getCompany(job.companyId);
+            if (!company)
+                throw notFoundError("No Company with ID:" + args.id)
+            return company;
         }
     },
     Company: {
-        jobs: (company)=> getJobsByCompany(company.id)
+        jobs: async (company) => {
+            const job = await getJobsByCompany(company.id);
+            if (!job)
+                throw notFoundError("No Job with ID:" + args.id)
+            return job;
+        }
+    },
+    Mutation: {
+        createJob: async (_root, { input: { title, description } }, { user }) => {
+            if (!user) throw unAuthorizedError("jwt auth failed")
+            return createJob({ companyId: user.companyId, title, description });
+        },
+        deleteJob: async (_root, { id }, { user }) => {
+            if (!user) throw unAuthorizedError("jwt auth failed");
+            const job = await getJob(id);
+            if (job.companyId !== user.companyId) throw unAuthorizedError("Invalid user: Not your job")
+            return deleteJob(id);
+        },
+        updateJob: async (_root, { input }, { user }) => {
+            if (!user) throw unAuthorizedError("jwt auth failed");
+            const job = await getJob(id);
+            if (job.companyId !== user.companyId) throw unAuthorizedError("Invalid user: Not your job")
+            return updateJob(input)
+        }
     }
 }
 
 const notFoundError = (message) => {
     return new GraphQLError(message, {
-        extensions: {code: "NOT_FOUND"}
+        extensions: { code: "NOT_FOUND" }
+    })
+}
+
+const unAuthorizedError = (message) => {
+    return new GraphQLError(message, {
+        extensions: { code: "UN_AUTHORIZED" }
     })
 }
--- job-board/server/server.js
+++ job-board/server/server.js
@@ -5,7 +5,7 @@ import { ApolloServer } from '@apollo/server';
 import { expressMiddleware as apolloMiddleware } from '@apollo/server/express4';
 import { readFile } from "node:fs/promises"
 import { resolvers } from './resolver.js';
-import { isUtf8 } from 'node:buffer';
+import { getUser } from './db/users.js';
 
 const PORT = 9000;
 
@@ -14,11 +14,18 @@ const app = express();
 app.use(cors(), express.json(), authMiddleware);
 app.post('/login', handleLogin);
 
+async function getContext({req}) {
+  if(req.auth){
+    const user = await getUser(req.auth.sub);
+    return {user}
+  }
+  return {}
+}
 //apolo server setup
 const typeDefs = await readFile('./schema.graphql', 'utf8');
 const apolloServe = new ApolloServer({ typeDefs, resolvers });
 await apolloServe.start();
-app.use('/graphql', apolloMiddleware(apolloServe));
+app.use('/graphql', apolloMiddleware(apolloServe, {context: getContext}));
 
 app.listen({ port: PORT }, () => {
   console.log(`Server running on port ${PORT}`);
